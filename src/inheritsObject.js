4f4c9fc871c029c6218dddc7d4fd45c9921d2888
Riceball LEE <snowyu.lee@gmail.com>
2015-07-10 18:10:35 +0800

    + use the new EC6 Object.getPrototypeOf, Object.setPrototypeOf if possible.

--- src/inheritsObject.js
+++ src/inheritsObject.js
@@ -1,15 +1,29 @@
 var inherits = require('./inherits');
 
 //make sure the aClass.prototype hook to the aObject instance.
+
+var getPrototypeOf = Object.getPrototypeOf;
+if (!getPrototypeOf) {
+  getPrototypeOf = function(obj) {
+    return obj.__proto__;
+  };
+}
+
+var setPrototypeOf = Object.setPrototypeOf;
+if (!setPrototypeOf) {
+  setPrototypeOf = function(obj, prototype) {
+    obj.__proto__ = prototype;
+  };
+}
+
 module.exports = function(aObject, aClass) {
   // ES6: Object.getPrototypeOf / Object.setPrototypeOf
-  var vOldProto = aObject.__proto__;
+  var vOldProto = getPrototypeOf(aObject);
   var result = false;
   if ( vOldProto !== aClass.prototype) {
     inherits(aClass, vOldProto.constructor);
-    aObject.__proto__ = aClass.prototype;
+    setPrototypeOf(aObject, aClass.prototype);
     result = true;
   }
   return result;
-}
-
+};
