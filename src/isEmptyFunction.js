26a3916c404e0877186d994e67b3af06bd2d8f37
Riceball LEE <snowyu.lee@gmail.com>
2015-07-23 21:01:59 +0800

    + isEmptyFunction should support istanbul(code coverage) hooked empty function

--- src/isEmptyFunction.js
+++ src/isEmptyFunction.js
@@ -1,4 +1,10 @@
-module.exports = function(aFunc) {
-  var result = /^function\s*\S*\s*\(.*\)\s*{[\s;]*}$/.test(aFunc.toString());
+module.exports = function(aFunc, istanbul) {
+  var vStr = aFunc.toString();
+  var result = /^function\s*\S*\s*\(.*\)\s*{[\s;]*}$/.test(vStr);
+  if (!result) {
+    if (!istanbul) try {istanbul = require('istanbul');} catch(e){};
+    if (istanbul)
+      result = /^function\s*\S*\s*\(.*\)\s*{__cov_[\d\w_]+\.f\[.*\]\+\+;}$/.test(vStr);
+  }
   return result;
 }
