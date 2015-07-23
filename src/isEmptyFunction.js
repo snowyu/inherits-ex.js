70406e34ff7ae227bfd3965f160493bf8bea601b
Riceball LEE <snowyu.lee@gmail.com>
2015-07-23 22:10:39 +0800

    * [bug] the node >=0.12 function.toString() add a empty multi-line comments, to make isEmptyFunction not work.

--- src/isEmptyFunction.js
+++ src/isEmptyFunction.js
@@ -1,10 +1,10 @@
 module.exports = function(aFunc, istanbul) {
   var vStr = aFunc.toString();
-  var result = /^function\s*\S*\s*\(.*\)\s*{[\s;]*}$/.test(vStr);
+  var result = /^function\s*\S*\s*\((.|[\n\r\u2028\u2029])*\)\s*{[\s;]*}$/g.test(vStr);
   if (!result) {
     if (!istanbul) try {istanbul = require('istanbul');} catch(e){};
     if (istanbul)
-      result = /^function\s*\S*\s*\(.*\)\s*{__cov_[\d\w_]+\.f\[.*\]\+\+;}$/.test(vStr);
+      result = /^function\s*\S*\s*\((.|[\n\r\u2028\u2029])*\)\s*{__cov_[\d\w_]+\.f\[.*\]\+\+;}$/.test(vStr);
   }
   return result;
 }
