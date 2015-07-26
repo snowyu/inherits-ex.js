4e4192ce4269b3d6a188e7ec0ae33e6c9fee7a55
Riceball LEE <snowyu.lee@gmail.com>
2015-07-26 18:57:58 +0800

    * use external getPrototypeOf/setPrototypeOf functions

--- src/inheritsObject.js
+++ src/inheritsObject.js
@@ -1,20 +1,8 @@
 var inherits = require('./inherits');
+var getPrototypeOf = require('./getPrototypeOf');
+var setPrototypeOf = require('./setPrototypeOf');
 
 //make sure the aClass.prototype hook to the aObject instance.
-
-var getPrototypeOf = Object.getPrototypeOf;
-if (!getPrototypeOf) {
-  getPrototypeOf = function(obj) {
-    return obj.__proto__;
-  };
-}
-
-var setPrototypeOf = Object.setPrototypeOf;
-if (!setPrototypeOf) {
-  setPrototypeOf = function(obj, prototype) {
-    obj.__proto__ = prototype;
-  };
-}
 
 module.exports = function(aObject, aClass) {
   // ES6: Object.getPrototypeOf / Object.setPrototypeOf
