8a90ea56cb30684c4c5fc0967c656d845eb81dd4
Riceball LEE <snowyu.lee@gmail.com>
2021-04-16 21:37:17 +0800

    fix(isEmptyFunction): add ES6 Class constructor empty check

--- src/isEmptyFunction.js
+++ src/isEmptyFunction.js
@@ -2,6 +2,12 @@ module.exports = function(aFunc, istanbul) {
   var vStr = aFunc.toString();
   var result = /^function\s*\S*\s*\((.|[\n\r\u2028\u2029])*\)\s*{[\s;]*}$/g.test(vStr);
   if (!result) {
+    result = /^class\s+\S+\s*{\s*}/g.test(vStr);
+  }
+  if (!result) {
+    result = /^class\s+\S+\s*{\s*(\S+\s*\n\s*)?constructor\s*\(.*\)\s*{\s*}}/g.test(vStr);
+  }
+  if (!result) {
     if (!istanbul) try {istanbul = eval("require('istanbul')");} catch(e){}
     if (istanbul)
       result = /^function\s*\S*\s*\((.|[\n\r\u2028\u2029])*\)\s*{__cov_[\d\w_]+\.f\[.*\]\+\+;}$/.test(vStr);
