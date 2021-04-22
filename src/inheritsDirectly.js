c1dc3e6850e9df021ac04b3b0b4e0e1a731048e4
Riceball LEE <snowyu.lee@gmail.com>
2021-04-22 22:41:54 +0800

    fix(inheritsDirectly): can not replace the prototype of ES6 class

--- src/inheritsDirectly.js
+++ src/inheritsDirectly.js
@@ -6,11 +6,16 @@ var defineProperty = require('./defineProperty');
 module.exports = function(ctor, superCtor, staticInherit) {
   defineProperty(ctor, 'super_', superCtor);
   defineProperty(ctor, '__super__', superCtor.prototype);//for coffeeScript super keyword.
-  // console.log('TCL:: ~ file: inheritsDirectly.js ~ line 9 ~ ctor', ctor);
-  ctor.prototype = newPrototype(superCtor, ctor);
+  var vPrototype = newPrototype(superCtor, ctor);
+  ctor.prototype = vPrototype; // ES6 class can not modify prototype!
+  if (vPrototype !== ctor.prototype) {
+    defineProperty(ctor.prototype, 'constructor', vPrototype.constructor)
+    defineProperty(ctor.prototype, 'Class', vPrototype.Class)
+  }
+  // console.log('TCL:: ~ file: inheritsDirectly.js ~ line 11 ~ ctor.prototype', ctor.prototype, ctor.prototype.constructor, ctor.prototype.Class);
   setPrototypeOf(ctor.prototype, superCtor.prototype);
   if (staticInherit !== false) {
     // NOTE: ES6 use this to keep superCtor.
-    setPrototypeOf(ctor, superCtor);//additional static inheritance
+    setPrototypeOf(ctor, superCtor); // additional static inheritance
   }
 };
