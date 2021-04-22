77becdafb82562320e60c37d7d2b46a5a836b9df
Riceball LEE <snowyu.lee@gmail.com>
2021-04-22 22:40:19 +0800

    fix(isEmptyFunction): check empty ES6 class correctly

--- src/isEmptyFunction.js
+++ src/isEmptyFunction.js
@@ -1,12 +1,10 @@
+var isEmptyCtor = require('./isEmptyCtor');
+
 module.exports = function(aFunc, istanbul) {
   var vStr = aFunc.toString();
   var result = /^function\s*\S*\s*\((.|[\n\r\u2028\u2029])*\)\s*{[\s;]*}$/g.test(vStr);
-  if (!result) {
-    result = /^class\s+\S+\s*{\s*}/g.test(vStr);
-  }
-  if (!result) {
-    result = /^class\s+\S+\s*{\s*(\S+\s*\n\s*)?constructor\s*\(.*\)\s*{\s*}}/g.test(vStr);
-  }
+  if (!result) {result = isEmptyCtor(vStr)}
+
   if (!result) {
     if (!istanbul) try {istanbul = eval("require('istanbul')");} catch(e){}
     if (istanbul)
