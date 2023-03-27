e14b8da62a21ab29816d9a417b63756f4282c353
Riceball LEE <snowyu.lee@gmail.com>
2023-03-27 16:28:55 +0800

    feat: add getOwnPropValue func

---
+++ src/getOwnPropValue.js
@@ -0,0 +1,16 @@
+const hasOwnProperty      = Object.prototype.hasOwnProperty;
+
+/**
+ * Returns the value of an own property of an object.
+ *
+ * @param {Object} obj - The object to get the property value from.
+ * @param {string} prop - The name of the property to get the value of.
+ * @returns {*} - The value of the property if it exists, undefined otherwise.
+ */
+export function getOwnPropValue(obj, prop) {
+  if (obj && hasOwnProperty.call(obj, prop)) {
+    return obj[prop];
+  }
+}
+
+export default getOwnPropValue;
