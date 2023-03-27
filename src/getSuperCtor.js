6b75e8fb6087bbe57aca6cc5a3a7eaea58893797
Riceball LEE <snowyu.lee@gmail.com>
2023-03-27 16:28:31 +0800

    feat: add getSuperCtor function

---
+++ src/getSuperCtor.js
@@ -0,0 +1,19 @@
+import getOwnPropValue from "./getOwnPropValue";
+
+const getPrototypeOf      = Object.getPrototypeOf;
+
+/**
+ * Return the Super Constructor of the ctor
+ * @param {Function} ctor
+ * @returns {Function}
+ */
+export function getSuperCtor(ctor) {
+  let ctorSuper = getOwnPropValue(ctor, 'super_');
+  if (!ctorSuper) {
+    const ctorProto = ctor.prototype && getPrototypeOf(ctor.prototype);
+    ctorSuper = ctorProto && (getOwnPropValue(ctorProto, 'Class') || ctorProto.constructor);
+  }
+  return ctorSuper;
+}
+
+export default getSuperCtor;
