22fa8d7326dbca5de0f1f23e8d490fd26feba197
Riceball LEE <snowyu.lee@gmail.com>
2023-03-27 16:30:07 +0800

    feat: add _clone function

---
+++ src/_clone.js
@@ -0,0 +1,30 @@
+const getOwnPropertyNames = Object.getOwnPropertyNames;
+
+/**
+ *
+ * @callback CloneFilterFn
+ * @param {string} name
+ * @param {any} value
+ * @returns {*} return value if include
+ */
+
+/**
+ * Clone own properties from src to dest
+ *
+ * @param {object} dest
+ * @param {object} src
+ * @param {CloneFilterFn} filter
+ * @returns {object} the dest object
+ */
+export function _clone(dest, src, filter) {
+  const names = getOwnPropertyNames(src);
+
+  for (let i = 0; i < names.length; i++ ) {
+    const k = names[i];
+    const value = filter(k, src[k]);
+    if (value !== undefined) {dest[k] = value;}
+  }
+  return dest;
+}
+
+export default _clone;
